e8c12468b0400208c548250fb9ff3398f234a3ef
Mohamed A. Othman <mohamedashrafothman@gmail.com>
2018-06-06 23:49:31 +0200

    add trips and single company to api

--- controllers/api.js
+++ controllers/api.js
@@ -2,29 +2,72 @@ const Company = require('../models/user');
 const Trip    = require('../models/trip');
 const Review  = require('../models/review');
 
-
-module.exports.getCompanies = async (req, res, next) => {
-	const companies = await Company.find().where('role').equals('admin').populate('trips').populate('reviews').exec();
-	res.json(companies);
-}
-
-module.exports.getcompany = async (req, res, next) => {
-	const company = await Company.findOne({
-		slug: req.params.company
-	}).populate('trips').populate('reviews').exec();
-	res.json(company);
-};
-
-
-module.exports.getTrips = async (req, res, next) => {
-	const trips = await Trip.find().where('removed').equals('false').populate('author').exec();
-	res.json(trips);
-};
-
-module.exports.getHomePAge = async (req, res, next)=> {
-	const companies = await Company.find().where('role').equals('admin').select('name _id photo').exec();
-	for(var i = 0 ; i < companies.length; i++){
-		companies[i].photo = `http://${req.host}/img/uploads/${companies[i].photo}`;
+module.exports = {
+	getCompanies: async (req, res, next) => {
+		const companies = await Company
+			.find()
+			.where('role')
+			.equals('admin')
+			.populate('trips')
+			.populate('reviews')
+			.select('name _id photo trips reviews')
+			.exec();
+		for (var i = 0; i < companies.length; i++) {
+			companies[i].photo = `http://${req.host}/img/uploads/${companies[i].photo}`;
+		}
+		res.json(companies);
+	},
+	getCompany: async (req, res, next) => {
+		const company = await Company
+			.findOne({
+				_id: req.params.company
+			})
+			.populate('trips')
+			.populate('reviews')
+			.exec();
+		company.photo = `http://${req.host}/img/uploads/${company.photo}`;
+		res.json(company);
+	},
+	getCompanyTrips: async (req, res, next) => {
+		const trips = await Trip
+			.find()
+			.where('author')
+			.equals(req.params.company)
+			.populate('requests')
+			.populate('author')
+			.exec();
+		res.json(trips);
+	},
+	getCompanySingleTrip: async (req, res, next) => {
+		const trip = await Trip
+			.findOne({
+				_id: req.params.trip
+			})
+			.populate('author')
+			.populate('requests')
+			.exec();
+		res.json(trip);
+	},
+	getTrips: async (req, res, next) => {
+		const trips = await Trip
+			.find()
+			.where('removed')
+			.equals('false')
+			.populate('author')
+			.exec();
+		res.json(trips);
+	},
+	getHomePAge: async (req, res, next) => {
+		const companies = await Company
+			.find()
+			.where('role')
+			.equals('admin')
+			.select('name _id photo')
+			.exec();
+		for (var i = 0; i < companies.length; i++) {
+			companies[i].photo = `http://${req.host}/img/uploads/${companies[i].photo}`;
+		}
+		res.json(companies);
 	}
-	res.json(companies);
+
 };
--- routes/api.js
+++ routes/api.js
@@ -7,8 +7,10 @@ const { requireRole } = require('../handlers/roleHandlers');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 
-router.get('/companies', catchErrors(apiController.getCompanies));
-router.get('/company/:company', catchErrors(apiController.getcompany));
+router.get('/company', catchErrors(apiController.getCompanies));
+router.get('/company/:company', catchErrors(apiController.getCompany));
+router.get('/company/:company/trips', catchErrors(apiController.getCompanyTrips));
+router.get('/company/:company/trips/:trip', catchErrors(apiController.getCompanySingleTrip));
 router.get('/trips', catchErrors(apiController.getTrips));
 router.get('/home', catchErrors(apiController.getHomePAge));
 module.exports = router;
